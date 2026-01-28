0e48756cd1ceba2b7b8a286b568ecaa8f3ef34b6
Pedro Thomas <124922008+Prthomas808@users.noreply.github.com>
2026-01-28 12:01:12 -0500

    register form and login form created

--- job_tracker/core/urls.py
+++ job_tracker/core/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_view
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("register/", views.register_view, name="register"),
+    path("login/", auth_view.LoginView.as_view(template_name="core/auth/login.html"), name="login"),
 ]
--- job_tracker/core/views.py
+++ job_tracker/core/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from .forms import RegisterForm
 
 # Create your views here.
 def index(request):
     return render(request, "core/index.html")
 
+def register_view(request):
+    if request.POST == "Post":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("index")
+    else:
+        form = RegisterForm()
+
+    return render(request, "core/auth/register.html", {"form" : form})
